5623b44764a1b5a3ad9b52d82a602e8ab70e05e9
Svetli280305 <syotov2021@elam.co.uk>
2022-01-19 09:31:37 +0000

    game

---
+++ Over and Over/Assets/Play Scene/CameraController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Create a public reference to the player - we will assign this using the unity editor
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Change our position relative to the players position
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+    }
+}
