a09fcf9e738dc9a2b3876270e94f88e690eac091
Svetoslav Yotov <syotov2021@elam.co.uk>
2022-01-19 15:07:03 +0000

    gg

--- Over and Over/Assets/Play Scene/PlayerInteraction.cs
+++ Over and Over/Assets/Play Scene/PlayerInteraction.cs
@@ -11,7 +11,7 @@ public class PlayerInteraction : MonoBehaviour
     {
 
 
-        if (Input.GetKeyDown("f ") && currentInterObj)
+        if (Input.GetKeyDown(KeyCode.F) && currentInterObj)
         {
             Debug.Log("F is pressed");
              //Check to see if this object talks and has a message
@@ -19,7 +19,6 @@ public class PlayerInteraction : MonoBehaviour
             {
                 //Tell the object to give its message
                 currentInterObjScript.Talk();
-                Debug.Log("talks");
             }
         }
     }
