b090506cbf8f1dda94b5cb1bb9d855a8dbecb712
Svetli280305 <syotov2021@elam.co.uk>
2022-02-10 15:01:10 +0000

    gg

--- Over and Over v2/Assets/Play Scenes/CameraController.cs
+++ Over and Over v2/Assets/Play Scenes/CameraController.cs
@@ -6,11 +6,15 @@ public class CameraController : MonoBehaviour
 {
     //Create a public reference to the player - we will assign this using the unity editor
     public GameObject player;
+    public float xOffset = 0;
+    public float yOffset = 0;
+    public float leftmax = 0;
+    public float rightmax = 0;
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
     {
         //Change our position relative to the players position
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(player.transform.position.x + xOffset, leftmax, rightmax), player.transform.position.y + yOffset, transform.position.z);
     }
 }
